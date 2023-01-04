ab8e9f53197607b6c8177f5f1bd7cd4bdb6ef884
zmr89 <marsell.ru@gmail.com>
2023-01-04 17:02:33 +0500

    Creating POJO

---
+++ app/src/main/java/com/example/cryptoinfo/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.cryptoinfo.api
+
+interface ApiService {
+}
---
+++ app/src/main/java/com/example/cryptoinfo/pojo/TopListResponse.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoinfo.pojo
+
+import com.google.gson.annotations.Expose
+
+import com.google.gson.annotations.SerializedName
+
+
+data class TopListResponse (
+    @SerializedName("Data")
+    @Expose
+    val data: List<Datum>? = null
+)
